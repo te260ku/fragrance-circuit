1378bd780e69913ed36a9a77ed46c6987d0fc021
te26Oku <teturou@aw.wakwak.com>
2020-08-25 20:58:57 +0900

    27個のランドに対応してサーボが回転するようにした

--- olfactory_display/od_integrated/od_integrated.ino
+++ olfactory_display/od_integrated/od_integrated.ino
@@ -20,13 +20,44 @@ void ServoUp(Servo s) {
 
 void setup() {
   Serial.begin(9600);
-  // 基板デバイスで使うピンの設定
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+  
+  // デジタルピン
+  pinMode(2, INPUT);  
+  pinMode(3, INPUT);  
+  pinMode(4, INPUT);
+  pinMode(14, INPUT);
+  pinMode(15, INPUT);  
+  pinMode(16, INPUT);  
+  pinMode(17, INPUT);  
+  pinMode(18, INPUT); 
+  pinMode(22, INPUT);  
+  pinMode(23, INPUT);  
+  pinMode(24, INPUT);  
+  pinMode(25, INPUT);  
+  pinMode(26, INPUT);  
+  pinMode(27, INPUT);  
+  pinMode(35, INPUT);
+  pinMode(40, INPUT);  
+  pinMode(41, INPUT);  
+  pinMode(42, INPUT);  
+  pinMode(43, INPUT); 
+  pinMode(44, INPUT);
+  pinMode(45, INPUT);
+  pinMode(46, INPUT);  
+  pinMode(47, INPUT);  
+  pinMode(48, INPUT);  
+  pinMode(49, INPUT);  
+  pinMode(50, INPUT);
+  pinMode(51, INPUT);
+  pinMode(52, INPUT);  
+  pinMode(53, INPUT); 
 
-  servo1.attach(5);
-  servo2.attach(6);
 
+  // サーボ
+  servo1.attach(13);
+  servo2.attach(12);
+
+  // サーボの初期化
   ServoDown(servo1);
   ServoDown(servo2);
 }
@@ -62,69 +93,62 @@ void SetLand(int landNum) {
 }
 
 void loop() {
-  
-  if (digitalRead(21) == HIGH) {
+  if (digitalRead(3) == HIGH) {
     SetLand(0);
-  } else if (digitalRead(22) == HIGH) {
+  } else if (digitalRead(4) == HIGH) {
     SetLand(1);
-  } else if (digitalRead(23) == HIGH) {
+  } else if (digitalRead(14) == HIGH) {
     SetLand(2);
-  } else if (digitalRead(24) == HIGH) {
+  } else if (digitalRead(15) == HIGH) {
     SetLand(3);
-  } else if (digitalRead(25) == HIGH) {
+  } else if (digitalRead(16) == HIGH) {
     SetLand(4);
-  } else if (digitalRead(26) == HIGH) {
+  } else if (digitalRead(17) == HIGH) {
     SetLand(5);
-  } else if (digitalRead(27) == HIGH) {
+  } else if (digitalRead(18) == HIGH) {
     SetLand(6);
-  } else if (digitalRead(28) == HIGH) {
+  } else if (digitalRead(22) == HIGH) {
     SetLand(7);
-  } else if (digitalRead(29) == HIGH) {
+  } else if (digitalRead(23) == HIGH) {
     SetLand(8);
-  } else if (digitalRead(30) == HIGH) {
+  } else if (digitalRead(24) == HIGH) {
     SetLand(9);
-  } else if (digitalRead(31) == HIGH) {
+  } else if (digitalRead(25) == HIGH) {
     SetLand(10);
-  } else if (digitalRead(32) == HIGH) {
+  } else if (digitalRead(26) == HIGH) {
     SetLand(11);
-  } else if (digitalRead(33) == HIGH) {
+  } else if (digitalRead(27) == HIGH) {
     SetLand(12);
-  } else if (digitalRead(34) == HIGH) {
-    SetLand(13);
   } else if (digitalRead(35) == HIGH) {
-    SetLand(14);
-  } else if (digitalRead(36) == HIGH) {
-    SetLand(15);
-  } else if (digitalRead(37) == HIGH) {
-    SetLand(16);
-  } else if (digitalRead(38) == HIGH) {
-    SetLand(17);
-  } else if (digitalRead(39) == HIGH) {
-    SetLand(18);
+    SetLand(13);
   } else if (digitalRead(40) == HIGH) {
-    SetLand(19);
+    SetLand(14);
   } else if (digitalRead(41) == HIGH) {
-    SetLand(20);
+    SetLand(15);
   } else if (digitalRead(42) == HIGH) {
-    SetLand(21);
+    SetLand(16);
   } else if (digitalRead(43) == HIGH) {
-    SetLand(22);
+    SetLand(17);
   } else if (digitalRead(44) == HIGH) {
-    SetLand(23);
+    SetLand(18);
   } else if (digitalRead(45) == HIGH) {
-    SetLand(24);
+    SetLand(19);
   } else if (digitalRead(46) == HIGH) {
-    SetLand(25);
+    SetLand(20);
   } else if (digitalRead(47) == HIGH) {
-    SetLand(26);
+    SetLand(21);
   } else if (digitalRead(48) == HIGH) {
-    SetLand(27);
+    SetLand(22);
   } else if (digitalRead(49) == HIGH) {
-    SetLand(28);
+    SetLand(23);
   } else if (digitalRead(50) == HIGH) {
-    SetLand(29);
+    SetLand(24);
   } else if (digitalRead(51) == HIGH) {
-    SetLand(30);
+    SetLand(25);
+  } else if (digitalRead(52) == HIGH) {
+    SetLand(26);
+  } else if (digitalRead(53) == HIGH) {
+    SetLand(27);
   } else {
     ServoDown(servo1);
     ServoDown(servo2);
