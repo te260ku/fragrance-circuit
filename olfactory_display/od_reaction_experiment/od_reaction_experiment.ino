beb9b1aabbb723218ef6cb25584e71f479cd3e51
te26Oku <teturou@aw.wakwak.com>
2020-09-11 16:18:48 +0900

    シリアル出力したときにインターバルを読み取りやすいように表記を修正

--- olfactory_display/od_reaction_experiment/od_reaction_experiment.ino
+++ olfactory_display/od_reaction_experiment/od_reaction_experiment.ino
@@ -2,41 +2,55 @@
  
 Servo servo1;
 Servo servo2;
+int c = 0;
 
 void setup(void){  
   servo1.attach(13); 
   servo2.attach(12); 
-  servo1.write(0); 
-  servo2.write(90);
+  servo1.write(80); 
+  servo2.write(0);
   
   
   Serial.begin(9600);
-  Serial.println("init");
+  Serial.println("Start");
 }
  
 void loop(void){
 
-  int startDelayRand = random(1, 5);
-  Serial.println(randDelay*1000);
-  delay(randDelay*1000);  
+  int startDelayRand = random(1, 3);
+  Serial.println("start delay: " + startDelayRand*1000);
+  delay(startDelayRand*1000);  
+
   
   int servoRand = random(10);
-  if (randServo < 5) {
+  if (servoRand < 5) {
     servo1.write(0); 
-    servo2.write(90);
+    servo2.write(0);
     Serial.println("1");
   } else {
-    servo1.write(90);
-    servo2.write(0);
-    Serial.println("2");
+    servo1.write(80);
+    servo2.write(70);
+    Serial.println("0");
   }
 
-  int endDelayRand = random(1, 5);
-  Serial.println(randDelay*1000);
-  delay(randDelay*1000);
+  int endDelayRand = random(1, 3);
+  Serial.println("end delay: " + endDelayRand*1000);
+  delay(endDelayRand*1000);
 
-  servo1.write(0);
-  servo1.write(90);
+
+
+  servo1.write(80);
+  servo2.write(0);
+  
+  
+  c++;
+  if (c > 5) {
+  delay(6000);
+  c = 0; 
+}
+
+
+  
 
   
 }
